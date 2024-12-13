937abcee178c084e0779edc11f318d7771e0fb8e
JAVP013 <joalvaldezpo@ittepic.edu.mx>
2024-12-12 21:26:26 -0700

    .

--- src/index.js
+++ src/index.js
@@ -9,35 +9,34 @@ const userResolvers = require('./resolvers/userResolver');
 const carritoTypeDefs = require('./schemas/shoppingCartSchema');
 const carritoResolvers = require('./resolvers/shoppingCartResolver');
 
-
 const startServer = async () => {
   // Conectar a MongoDB
-  await mongoose.connect('mongodb+srv://admin:admin@cluster0.yelht.mongodb.net/tet01?retryWrites=true&w=majority&appName=Cluster0');
+  try {
+    await mongoose.connect('mongodb+srv://admin:admin@cluster0.yelht.mongodb.net/tet01?retryWrites=true&w=majority&appName=Cluster0', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Conexión exitosa a MongoDB');
+  } catch (error) {
+    console.error('Error al conectar a MongoDB:', error);
+    process.exit(1);
+  }
 
   const server = new ApolloServer({
     typeDefs: [userTypeDefs, productTypeDefs, carritoTypeDefs],
     resolvers: [userResolvers, productResolvers, carritoResolvers],
     introspection: true, // Permite que el esquema sea consultado en producción
     playground: true, // Activa la consola en producción (Apollo Studio)
+    cors: {
+      origin: 'https://carrito-d53913b23d26.herokuapp.com', // Permitir solo esta URL
+      credentials: true,
+    },
   });
 
-  app.use((req, res, next) => {
-    const allowedOrigins = ['https://carrito-d53913b23d26.herokuapp.com'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+  const PORT = process.env.PORT || 4000;
 
-
-    const PORT = process.env.PORT || 4000;
-
-    server.listen(PORT, () => {
-      console.log(`Servidor corriendo en el puerto ${PORT}`);
-  
+  server.listen(PORT).then(({ url }) => {
+    console.log(`Servidor corriendo en ${url}`);
   });
 };
 
