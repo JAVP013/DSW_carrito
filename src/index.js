1f7379fe53aa2e0b218e1a1beebb17b5dd1ad8be
JAVP013 <joalvaldezpo@ittepic.edu.mx>
2024-12-12 21:38:12 -0700

    .

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
+require('dotenv').config();  // Cargar las variables de entorno desde el archivo .env
 
 // Importar los esquemas y resolvers
 const productTypeDefs = require('./schemas/productSchema');
@@ -9,10 +10,11 @@ const userResolvers = require('./resolvers/userResolver');
 const carritoTypeDefs = require('./schemas/shoppingCartSchema');
 const carritoResolvers = require('./resolvers/shoppingCartResolver');
 
+// Función para iniciar el servidor
 const startServer = async () => {
-  // Conectar a MongoDB
+  // Conectar a MongoDB utilizando la URI almacenada en las variables de entorno
   try {
-    await mongoose.connect('mongodb+srv://admin:admin@cluster0.yelht.mongodb.net/tet01?retryWrites=true&w=majority&appName=Cluster0', {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -22,22 +24,26 @@ const startServer = async () => {
     process.exit(1);
   }
 
+  // Configuración del servidor Apollo
   const server = new ApolloServer({
     typeDefs: [userTypeDefs, productTypeDefs, carritoTypeDefs],
     resolvers: [userResolvers, productResolvers, carritoResolvers],
-    introspection: true, // Permite que el esquema sea consultado en producción
-    playground: true, // Activa la consola en producción (Apollo Studio)
+    introspection: process.env.NODE_ENV === 'development',  // Solo en desarrollo
+    playground: process.env.NODE_ENV === 'development',   // Solo en desarrollo
     cors: {
-      origin: 'https://carrito-d53913b23d26.herokuapp.com', // Permitir solo esta URL
+      origin: process.env.CORS_ORIGIN.split(','),  // Permitir orígenes configurados en las variables de entorno
       credentials: true,
     },
   });
 
+  // Puerto donde se ejecutará el servidor
   const PORT = process.env.PORT || 4000;
 
+  // Iniciar el servidor Apollo
   server.listen(PORT).then(({ url }) => {
     console.log(`Servidor corriendo en ${url}`);
   });
 };
 
+// Iniciar el servidor
 startServer();
